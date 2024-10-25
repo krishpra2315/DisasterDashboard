cb2bf652ea7d9225d29f56cef9f4c7de0aeed6fd
Krish Prasad <krishkp00@gmail.com>
2024-10-25 13:05:27 -0400

    Revert "Initial Commit"
    This reverts commit 59850c6a290d4ccc2fd9ebe45c61a46402c15713.

--- app/config.py
+++
@@ -1,4 +0,0 @@
-import os
-
-class Config:
-    MONGO_URI = os.getenv('MONGO_URI', 'mongodb+srv://kvem56:r1z7zYYAJCOGUoqf@clusterweatherapi.qxbok.mongodb.net/weather_db?retryWrites=true&w=majority&appName=ClusterWeatherAPI')
