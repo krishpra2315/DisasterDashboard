9535c2455012afffcd75b5596f00e6204a4e41ac
Krish Prasad <krishkp00@gmail.com>
2024-11-01 18:33:13 -0400

    return data

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, jsonify
-from pymongo import MongoClient
-from backend.app.config import Config
+import csv
+import os
 
 main = Blueprint('main', __name__)
 
@@ -11,18 +11,18 @@ def index():
 
 @main.route('/weather', strict_slashes=False, methods=['GET', 'POST'])
 def weather():
-    try:
-        client = MongoClient(Config.MONGO_URI)
-        db = client['weather_db']
-        weather_collection = db['weather']
-        print("Connected to MongoDB")
-        weather_data = list(weather_collection.find())
-        for data in weather_data:
-            data['_id'] = str(data['_id'])  # Convert ObjectId to string for JSON serialization
-        return jsonify(weather_data)
-    except Exception as e:
-        print(e)
 
-    return "<p>i suck</p>"
+    # Open the CSV file
+    cwd = os.getcwd()
+    with open('/Users/krishprasad/Desktop/Projects/DisasterDashboard/backend/storms.csv', mode='r') as file:
+        # Create a CSV reader
+        reader = csv.reader(file)
+        data = []
+        # Optionally, skip the header row
+        header = next(reader)
 
-    #return render_template('weather.html', weathers=list(weather_collection.find()))
+        # Read each row in the CSV file
+        for row in reader:
+            data.append(dict(zip(header, row)))
+
+    return data
