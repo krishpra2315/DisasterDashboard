8f1e7bc55dcef682c94e498b29708d0fb1517c40
Krish Prasad <krishkp00@gmail.com>
2025-01-04 10:35:09 -0600

    move important files to root

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,9 +1,6 @@
-from app import create_app
+from backend.app import create_app
 
 app = create_app()
 
-port = 5000
-
 if __name__ == '__main__':
-    print("app running on port {}".format(port))
-    app.run(debug=True, port=port)
+    app.run()
