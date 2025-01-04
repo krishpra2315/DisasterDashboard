470990098e5fc195d2a7fb349170391b3f5dd718
Krish Prasad <krishkp00@gmail.com>
2025-01-04 11:21:37 -0600

    Update __init__.py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,15 +2,10 @@ from flask import Flask
 import os
 
 def create_app():
-    project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..'))
-
-    print(f"project root: {project_root}")
-    # Set the correct path to the dist folder
-    static_folder_path = os.path.join(project_root, 'backend', 'app', 'static', 'dist')
 
     app = Flask(
         __name__,
-        static_folder=static_folder_path,
+        static_folder='static/dist',
         static_url_path="",
     )
 
